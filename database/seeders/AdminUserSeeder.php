26c0c193ad4d530afc556fde885c77baebb91277
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2024-08-07 20:42:51 +0300

    add seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::firstOrCreate(
+            ['email' => 'admin@alforqanschool.net'],
+            [
+                'password' => Hash::make('Admin@Forqan_2024'),
+                'name' => 'Admin',
+                'freezed' => 0,
+                'is_admin' => 1
+            ]
+        );
+    }
+}
