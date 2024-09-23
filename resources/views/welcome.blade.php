da168096c63c86651a6bf3a8d0fd1ecdd855e615
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2024-09-23 22:08:28 +0300

    title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Alforqan Web Application || مدرسة الفرقان</title>
+    <title>Dar Al-Hadith Web Application || مدرسة دار الحديث</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
