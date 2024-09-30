b47289c1ed9359a27b14cfd70837a4076473b76d
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2024-10-01 00:37:13 +0300

    edit video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
                 <img src="\images\iconwhite.png" style="max-height: 10vh; max-width: 10vw;" class="my-5">
             </div>
             <div class="grid grid-cols-1 justify-items-center">
-                <iframe class="aspect-video md:w-7/12 w-11/12" src="https://www.youtube.com/embed/DOHKEZ4LOfY?si=aWa6HcsCIXTEQSN5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe class="aspect-video md:w-7/12 w-11/12" src="https://www.youtube.com/embed/M3o5DrrgpKk?si=cZt6FWtKSumMU5ZO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div class="grid grid-cols-1 sm:grid-cols-3 md:w-7/12 w-11/12 justify-self-center">
                 <div class="m-2 p-2 sm:m-3 sm:p-5">
