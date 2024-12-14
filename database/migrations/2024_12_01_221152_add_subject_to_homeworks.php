0602f3e2709e33829584c59f1fd12b696c8ada71
Essam Dimashky <essam.dimashky@tatweer.sy>
2024-12-14 15:25:14 +0300

    fix migration

--- database/migrations/2024_12_01_221152_add_subject_to_homeworks.php
+++ database/migrations/2024_12_01_221152_add_subject_to_homeworks.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('homeworks', function (Blueprint $table) {
-            //
+            $table->string('subject');
         });
     }
 
